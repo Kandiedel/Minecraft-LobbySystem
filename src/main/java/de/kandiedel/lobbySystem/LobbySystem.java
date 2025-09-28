4a64919c5f3d08ff37ea82e12a3f0b237db3a651
Kandiedel <triebelfinn@gmail.com>
2025-09-28 03:23:01 +0200

    noTicket: Added enabled and disabled messages

--- src/main/java/de/kandiedel/lobbySystem/LobbySystem.java
+++ src/main/java/de/kandiedel/lobbySystem/LobbySystem.java
@@ -1,17 +1,31 @@
 package de.kandiedel.lobbySystem;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LobbySystem extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        getServer().getConsoleSender().sendMessage("");
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[✔] "
+                + ChatColor.DARK_PURPLE + getDescription().getName()
+                + ChatColor.GRAY + " wurde erfolgreich aktiviert!");
+        getServer().getConsoleSender().sendMessage("     "
+                + ChatColor.GRAY + "Entwickelt von "
+                + ChatColor.DARK_PURPLE + "Kandiedel 🛠️");
+        getServer().getConsoleSender().sendMessage("");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getServer().getConsoleSender().sendMessage("");
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "[✖] "
+                + ChatColor.DARK_PURPLE + getDescription().getName()
+                + ChatColor.GRAY + " wurde erfolgreich deaktiviert!");
+        getServer().getConsoleSender().sendMessage("     "
+                + ChatColor.GRAY + "Entwickelt von "
+                + ChatColor.DARK_PURPLE + "Kandiedel 🛠️");
+        getServer().getConsoleSender().sendMessage("");
     }
 }
